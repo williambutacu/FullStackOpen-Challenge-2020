f6faa53d1a59f71491119118ac0871634ff9a260
williambutacu <florin.butacu@stud.umfcd.ro>
2020-12-11 22:20:46 +0200

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,19 +6,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all,setAll]=useState(0)
 
 function handleGood(){
   setGood(good+1)
+  setAll(all+1)
+  
 }
 
 function handleNeutral(){
   setNeutral(neutral+1)
+  setAll(all+1)
+
 }
 
 function handleBad(){
   setBad(bad+1)
+  setAll(all+1)
+
 }
 
+let average = (good-bad)/all
+let positive = good/all
   return (
     <div>
       <h1>Give feedback</h1>
@@ -28,9 +37,13 @@ function handleBad(){
 <br>
 </br>
 <h1>Statistics</h1>
+<p>All {all}</p>
 <p>Good {good}</p>
 <p>Neutral {neutral}</p>
 <p>Bad {bad}</p>
+<p>Average {average}</p>
+
+<p>Positive {positive} %</p>
     </div>
   )
 }
